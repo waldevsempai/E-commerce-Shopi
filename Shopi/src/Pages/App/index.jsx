bd9e3c753e509ed7e38483175d0a71823c21fbf7
Walter Salas <waldev.salas@gmail.com>
2023-09-27 23:28:21 -0300

    Add Context

--- Shopi/src/Pages/App/index.jsx
+++ Shopi/src/Pages/App/index.jsx
@@ -1,4 +1,5 @@
 import { useRoutes, BrowserRouter } from 'react-router-dom'
+import { ShoppingCartProvider } from '../../Context'
 import Home from'../Home'
 import MyAccount from'../MyAccount'
 import MyOrder from'../MyOrder'
@@ -25,15 +26,14 @@ const AppRoutes = () =>{
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Navbar/>
-      <Layout>
-      <AppRoutes/>
-
-      </Layout>
-    
+    <ShoppingCartProvider>
+      <BrowserRouter>
+        <Navbar/>
+        <Layout>
+          <AppRoutes/>
+        </Layout>
     </BrowserRouter>
- 
+    </ShoppingCartProvider>
   )
 }
 
