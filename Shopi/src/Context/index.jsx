6c55f8cf7acce567d01bc01944f84ed5676ae4ec
Walter Salas <waldev.salas@gmail.com>
2023-10-19 22:40:42 -0300

    Add onClick ProductDetail

--- Shopi/src/Context/index.jsx
+++ Shopi/src/Context/index.jsx
@@ -5,11 +5,17 @@ export const ShoppingCartContext = createContext()
 // eslint-disable-next-line react/prop-types
 export const ShoppingCartProvider = ({children}) => {
     const [count, setCount] = useState(0)
+    const [ isProductDetailOpen, setIsProductDetailOpen] = useState(false)
+    const  openProductDetail = () => setIsProductDetailOpen(true)
+    const  closeProductDetail = () => setIsProductDetailOpen(false)
 
     return (
         <ShoppingCartContext.Provider value={{
           count,
-          setCount 
+          setCount,
+          openProductDetail,
+          closeProductDetail,
+          isProductDetailOpen
         }}>
             {children}
         </ShoppingCartContext.Provider>
