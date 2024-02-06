b0e28af66587760f5db43be5e581faf0aa43c8ff
Walter Salas <waldev.salas@gmail.com>
2024-02-06 01:49:25 -0300

    Add Search a Product

--- Shopi/src/Context/index.jsx
+++ Shopi/src/Context/index.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react'
+import { createContext, useState, useEffect } from 'react'
 
 export const ShoppingCartContext = createContext()
 
@@ -26,6 +26,18 @@ export const ShoppingCartProvider = ({children}) => {
     // Shopping Cart · Order
     const [order, setOrder] = useState([])
 
+
+    // Get Products
+    const [items, setItems] = useState(null)
+
+    // Get Products by title
+    const [searchByTitle, setSearchByTitle] = useState(null)
+
+    useEffect( () => {
+      fetch('https://api.escuelajs.co/api/v1/products')
+      .then(response => response.json())
+      .then(data => setItems(data))
+    }, [])
     return (
         <ShoppingCartContext.Provider value={{
           count,
@@ -41,7 +53,11 @@ export const ShoppingCartProvider = ({children}) => {
           openCheckoutSideMenu,
           closeCheckoutSideMenu,
           order,
-          setOrder
+          setOrder,
+          items,
+          setItems,
+          searchByTitle,
+          setSearchByTitle
         }}>
             {children}
         </ShoppingCartContext.Provider>
